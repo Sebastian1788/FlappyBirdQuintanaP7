9ad0c5216e3bf51cd7ee42ec15b6327949e48009
Sebastian1788 <145072446+Sebastian1788@users.noreply.github.com>
2023-12-06 15:25:44 -0800

    Unity
    I made the bird flap.

--- FlappyBirdQuintanaP7/Assets/bird.cs
+++ FlappyBirdQuintanaP7/Assets/bird.cs
@@ -8,10 +8,12 @@ public class bird : MonoBehaviour
 
     private bool isDead = false;
     private Rigidbody2D rb2d;
+    private Animator anim;
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D> ();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -22,7 +24,8 @@ public class bird : MonoBehaviour
             if (Input.GetMouseButtonDown (0))
             {
                 rb2d.velocity = Vector2.zero;
-                rb2d.AddForce(new Vector2(0, upforce));
+                rb2d.AddForce (new Vector2(0, upforce));
+                anim.SetTrigger("Flap");
             }
         }
            
@@ -30,5 +33,6 @@ public class bird : MonoBehaviour
     void OnCollisionEnter2D ()
     {
         isDead = true;
+        anim.SetTrigger ("Die");
     }
 }
